3e6374916a51bb39e1a7600d771970c9b6ac0e4a
Samuel Edwin Ward <sward@broward.edu>
2014-10-22 12:14:27 -0400

    fix dangling pointer The lifetime of the temporary QByteArray did not extend to the time when the pointer to its contents was accessed. Resolved by promoting the QByteArray to automatic storage duration.

--- Graphics/Shader.cpp
+++ Graphics/Shader.cpp
@@ -84,9 +84,10 @@ GLuint Shader::createShader(QString path, bool *success) {
         shouldCompile = false;
         *success = false;
     } else {
-        char *adapter[] = { f.readAll().data() };
+        QByteArray array = f.readAll();
+        const char *data = array.constData();
         shaderId = glCreateShader(type);
-        glShaderSource(shaderId, 1, adapter, 0);
+        glShaderSource(shaderId, 1, &data, NULL);
         if (!compileShader(shaderId)) {
             qWarning() << "Error on " << path;
             glDeleteShader(shaderId);
